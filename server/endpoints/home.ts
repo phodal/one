ef47c7a32a6ba62e9e55566630aca3a8cb695480
Phodal HUANG <h@phodal.com>
2019-05-20 20:31:07 +0800

    server: make callback works................

--- server/endpoints/home.ts
+++ server/endpoints/home.ts
@@ -1,12 +1,44 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
+import { APIGatewayProxyCallback, Callback, Context } from "aws-lambda";
 import 'source-map-support/register';
 
-export const handler: APIGatewayProxyHandler = async (event, _context) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify({
-      message: 'Go Serverless Webpack (Typescript) v1.0! Your function executed successfully!',
-      input: event,
-    }, null, 2),
+const AWS = require('aws-sdk');
+const shortid = require('shortid');
+
+AWS.config.setPromisesDependency(Promise);
+
+const tableName = process.env.DYNAMODB_TABLE;
+const docClient = new AWS.DynamoDB.DocumentClient();
+
+// @ts-ignore
+export const handler: APIGatewayProxyCallback = (event: any, context: Context, callback: Callback) => {
+  const timestamp = new Date().getTime();
+  const params = {
+    TableName: tableName,
+    Item: {
+      title: 'hello',
+      id: shortid.generate(),
+      createdAt: timestamp
+    }
   };
+
+  console.log('something for log test');
+  docClient.put(params, (error) => {
+    if (error) {
+      console.error(error);
+      callback(null, {
+        statusCode: error.statusCode || 501,
+        headers: {'Content-Type': 'text/plain'},
+        body: 'couldn\'t create the form item.',
+      });
+    }
+
+    console.log('aaa');
+    callback(null, {
+      statusCode: 201,
+      headers: {
+        "Access-Control-Allow-Origin": "*" // Required for CORS support to work
+      },
+      body: JSON.stringify(params.Item),
+    });
+  });
 };
